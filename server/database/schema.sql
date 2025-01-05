96ec00fef0854a39fb75dcd0f75a383ebdc31468
Hamza Maach <hamzamaach56@gmail.com>
2025-01-05 22:25:44 +0100

    change 'token_id' column name to 'token'

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS sessions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id BIGINT UNIQUE NOT NULL,
-    token_id TEXT NOT NULL,
+    token TEXT NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
